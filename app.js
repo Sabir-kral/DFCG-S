8233165afdf647b913c683c32c4b48ad9bec2721
Sabir-kral <sabirmemmedli21152014@gmail.com>
2025-03-15 18:17:46 +0400

    ez

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    let buton1 = document.querySelector(".show")
+    buton1.addEventListener("click",function () {
+        $(".item1").toggle(2000)
+    })
+    let buton2 = document.querySelector(".fadeout")
+    buton2.addEventListener("click",function () {
+        $(".item2").fadeOut(2000)
+    })
+    buton2.addEventListener("dblclick",function () {
+        $(".item2").fadeIn(2000)
+    })
+    let buton3 = document.querySelector(".fadeto")
+    buton3.addEventListener("click",function () {
+        $(".item3").fadeTo(2000, 0.25)
+    })
+    let buton4 = document.querySelector(".slide")
+    buton4.addEventListener("click",function () {
+        $(".item4").slideUp(2000)
+    })
+    buton4.addEventListener("dblclick",function () {
+        $(".item4").slideDown(2000)
+    })
+})
